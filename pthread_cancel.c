0c7b496607c63c8712933fd3e5621f818a57395d
zhangruochen77 <924945268@qq.com>
2023-06-03 21:07:28 +0800

    modify: pthread_cancel.c

--- pthread_cancel.c
+++ pthread_cancel.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
 
+void err(int errno) {
+	fprintf(stderr, "error, %sn", strerror(errno));
+	_exit(1);
+}
+
+void *func(void *arg) {
+	printf("pthread before sleep\n");
+	sleep(10);
+	printf("pthread after sleep\n");
+	pthread_exit(NULL);
+}
 
 int main() {
-	pthread_join()
+	pthread_t t;
+	int ret;
+	ret = pthread_create(&t, NULL, func, NULL);
+	if (0 != ret) {
+		err(ret);
+	}
+
+	sleep(2);
+	printf("to cancel thread %lu\n", t);
+	ret = pthread_cancel(t);
+	if (0 != ret) {
+		err(ret);
+	}
+
+	return 0;
 }
